50eaee96371fe1a7d1137286fba613617e290635
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-06-30 23:53:05 +0800

    Correct lib name.

--- build.clj
+++ build.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.build.api :as b]
             [clojure.java.io :as io]))
 
-(def lib 'com.berrysoft/berrysoft.data.parquet)
+(def lib 'io.github.berrysoft/berrysoft.data.parquet)
 (def version (format "0.1.0-%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
